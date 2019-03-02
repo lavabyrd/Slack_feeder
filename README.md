7e51e576c45599cd1e33a44b9d72b70956f8b9bb
splinterific <markgpreston@gmail.com>
2019-03-02 11:44:33 +0000

    added a feed adder function

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 Initialization of SlackFeeder
+
+
+# Helpful links
+- https://www.pythonforbeginners.com/feedparser/using-feedparser-in-python
+- https://api.slack.com
--- parse.py
+++ parse.py
@@ -4,13 +4,24 @@ import pprint
 feed = ["http://www.reddit.com/r/python/.rss"]
 
 
-for item in feed:
-    try:
-        parsed = feedparser.parse(item)
-        print(
-            f"{parsed.feed.title} feed successfully"
-            f"added from <url|{parsed.feed.link}>"
-        )
-    except:
-        print("that didn't work")
+def add_feed():
+    for item in feed:
+        try:
+            parsed = feedparser.parse(item)
+
+        except:
+            print("no items found")
+    print("successfull added all items")
+
+
+def feed_checker():
+    for item in feed:
+        try:
+            parsed = feedparser.parse(item)
+            print(
+                f"{parsed.feed.title} feed successfully "
+                f"added from <url|{parsed.feed.link}>"
+            )
+        except:
+            print("that didn't work")
 
