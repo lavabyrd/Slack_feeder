a79d6563138ffe6d3056006949a7fe6c688ea7e4
splinterific <markgpreston@gmail.com>
2019-03-02 11:24:01 +0000

    initializing of feeder app

---
+++ parse.py
@@ -0,0 +1,15 @@
+import feedparser
+import pprint
+
+feed = ["http://www.reddit.com/r/python/.rss"]
+
+
+for item in feed:
+    try:
+        parsed = feedparser.parse(item)
+        print(
+            f"{parsed.feed.title} feed successfully added from <url|{parsed.feed.link}>"
+        )
+    except:
+        print("that didn't work")
+
