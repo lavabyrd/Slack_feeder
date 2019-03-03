6e3619b330dcb731ab74c8baff39d45f6d42e5cf
splinterific <markgpreston@gmail.com>
2019-03-03 10:29:02 +0000

    updated to use html-slacker

--- parse.py
+++ parse.py
@@ -1,24 +1,15 @@
 import feedparser
 import pprint
 import os
-import re
+from htmlslacker import HTMLSlacker
 
 feed = ["http://www.reddit.com/r/python/.rss"]
-
-
-import re
-
-TAG_RE = re.compile(r'<[^>]+>')
-
-def remove_tags(text):
-    return TAG_RE.sub('', text)
 
 
 def add_feed():
     for item in feed:
         try:
             feedparser.parse(item)
-            
         except:
             print("no items found")
     print("successfull added all items")
@@ -32,7 +23,9 @@ def feed_checker():
                 f"{parsed.feed.title} feed successfully "
                 f"added from <url|{parsed.feed.link}>"
             )
-            print(TAG_RE.sub('', parsed.entries[0].summary))
+            outp = parsed.entries[0].summary
+            print(HTMLSlacker(outp).get_output())
+
         except:
             print("that didn't work")
 
