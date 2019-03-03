f5770b6f9750edb25a6d95fb842c946a88ca8583
splinterific <markgpreston@gmail.com>
2019-03-03 09:56:54 +0000

    adding parsing around the links to clean up the text

--- parse.py
+++ parse.py
@@ -1,14 +1,24 @@
 import feedparser
 import pprint
 import os
+import re
+
 feed = ["http://www.reddit.com/r/python/.rss"]
+
+
+import re
+
+TAG_RE = re.compile(r'<[^>]+>')
+
+def remove_tags(text):
+    return TAG_RE.sub('', text)
 
 
 def add_feed():
     for item in feed:
         try:
             feedparser.parse(item)
-
+            
         except:
             print("no items found")
     print("successfull added all items")
@@ -22,5 +32,9 @@ def feed_checker():
                 f"{parsed.feed.title} feed successfully "
                 f"added from <url|{parsed.feed.link}>"
             )
+            print(TAG_RE.sub('', parsed.entries[0].summary))
         except:
             print("that didn't work")
+
+
+feed_checker()
