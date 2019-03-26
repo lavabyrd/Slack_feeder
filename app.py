001da09c18d2764975576e186d8ed8f7296ef9e7
splinterific <markgpreston@gmail.com>
2019-03-26 08:28:36 +0000

    cleaned up app.py to remove debug

--- app.py
+++ app.py
@@ -9,12 +9,12 @@ app = Flask(__name__)
 app.register_blueprint(api)
 
 
-
-@app.route('/')
+@app.route("/")
 def home():
     return "hello"
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
 
-    app.run(debug=False,host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(host="0.0.0.0", port=port)
